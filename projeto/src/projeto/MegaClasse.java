ecaec30af8850af5c8c71abe076b4fc12b9b12bc
pmfae-iscte <pmfae@iscte-iul.pt>
2023-04-17 15:01:17 +0100

    Mega atualization

--- projeto/src/projeto/MegaClasse.java
+++ projeto/src/projeto/MegaClasse.java
@@ -3,5 +3,6 @@ package projeto;
 public class MegaClasse {
 	static public void gandaFunc() {
 		System.out.println("Ganda Stringue");
+		System.out.println("nha bro, este projeto tá granda projeto yah");
 	}
 }
